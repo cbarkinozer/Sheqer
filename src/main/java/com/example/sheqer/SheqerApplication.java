39f6f3c840da25a1bbe2ba15c0bd819f0e81656c
cbarkinozer <c.barkinozer@gmail.com>
2022-12-07 22:49:45 +0300

    feat: create project using the initialzr

---
+++ src/main/java/com/example/sheqer/SheqerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sheqer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheqerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheqerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sheqer/SheqerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sheqer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheqerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
